598369dc51daa4b01c96a509b24255487340936a
Christian Höltje <docwhat@gerf.org>
2018-02-26 22:22:24 -0500

    slack-style emojii strings :smiley:

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,7 @@ module.exports = {
           'gatsby-remark-prismjs',
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-smartypants',
+          'gatsby-remark-emoji',
         ],
       },
     },
