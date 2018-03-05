3518b1ad107f1799027e123913bb7fd09fcebb7d
Christian Höltje <docwhat@gerf.org>
2018-03-04 19:53:21 -0500

    mdl configuration

---
+++ .mdl-style.rb
@@ -0,0 +1,19 @@
+# MDL style
+# vim:set ft=ruby:
+
+all
+
+rule 'MD007', indent: 4
+rule 'MD029', style: 'ordered'
+rule 'MD004', style: :dash
+rule 'MD035', style: '---'
+
+exclude_rule 'first-header-h1'      # MD002
+exclude_rule 'fenced-code-language' # MD040
+exclude_rule 'first-line-h1'        # MD041
+exclude_rule 'header-style'         # MD029
+exclude_rule 'line-length'          # Line length
+exclude_rule 'list-marker-space'    # Spaces after list markers
+exclude_rule 'no-inline-html'       # MD033
+
+# EOF
