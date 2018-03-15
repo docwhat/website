fe36fa8ba33e8b43350694058d007447026f52c2
Christian Höltje <docwhat@gerf.org>
2018-03-15 19:15:17 -0400

    clean up comment form

---
+++ src/components/StyledButton.js
@@ -0,0 +1,44 @@
+('use strict');
+
+import React from 'react'
+import g from 'glamorous'
+import { rhythm } from '../utils/typography'
+import { heroColor } from '../utils/colors.js'
+
+const colors = {
+  danger: heroColor.hue(8),
+  primary: heroColor.darken(0.5),
+  secondary: heroColor.hue(198),
+  gray: heroColor.saturationl(20),
+}
+
+const StyledButton = g.button(
+  {
+    fontSize: 16,
+    margin: 10,
+    border: 'none',
+    cursor: 'pointer',
+    display: 'inline-block',
+    padding: '10px 20px',
+    textAlign: 'center',
+    transition: '0.25s cubic-bezier(0.17, 0.67, 0.52, 0.97)',
+    borderRadius: 4,
+    color: '#fff',
+    narf: heroColor.hue(),
+    boxShadow: '0 4px 6px rgba(50,50,93,.11), 0 1px 3px rgba(0,0,0,.08)',
+    ':hover': {
+      opacity: 0.7,
+      transform: 'translateY(-1px)',
+      boxShadow: '0 7px 14px rgba(50,50,93,.1), 0 3px 6px rgba(0,0,0,.08)'
+    },
+    ':focus': { outline: 0 },
+    ':active': {
+      transform: 'translateY(1px)'
+    }
+  },
+  props => ({
+    backgroundColor: (colors[props.type] || colors['primary']).string()
+  })
+)
+
+export default StyledButton
--- src/components/SubmitComment.js
+++ src/components/SubmitComment.js
@@ -1,9 +1,10 @@
+import g, { Div, Label, Input, Span, Form, Legend, Textarea } from 'glamorous'
 import React from 'react'
 import Helmet from 'react-helmet'
 import gray from 'gray-percentage'
 import { rhythm } from '../utils/typography'
 import { heroColor } from '../utils/colors.js'
-import PrimaryButton from './PrimaryButton.js'
+import StyledButton from './StyledButton.js'
 
 // TODO: Move the reCapcha into a separate component.
 // TODO: Get reCaptcha siteKey and secret from staticman.yml instead.
@@ -13,34 +14,48 @@ const FormOption = (props) => {
   const optParts = props.option.split('.')
   const name=`options[${optParts.join('][]')}]`
   return(
-    <input name={name} value={props.value} type="hidden" />
+    <Input name={name} value={props.value} type="hidden" />
   )
 }
 
 const Labelled = (props) => {
   var requiredText = ""
   if (!props.required) {
-    requiredText = <span
-      style={{ fontStyle: "italic", color: heroColor.darken(0.5).string() }}
-    >(optional)</span>
+    requiredText = <Span css={{
+      fontStyle: "italic",
+      color: heroColor.darken(0.5).string(),
+    }}
+  >(optional)</Span>
+  }
+
+  const css = props['css'] || {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    margin: "1em 0",
+  }
+
+  const divCss = props['divCss'] || {
+    display: "inline-block",
+    flex: "0 0 30%",
   }
   return (
-    <label><div
-        style={{  }}
-      >
+    <Label css={css}>
+      <Div css={divCss} >
         {props.label} {requiredText}
-      </div>
+      </Div>
       {props.children}
-    </label>
+    </Label>
   )
 }
 
 const LabelledInput = (props) => {
   return (
-    <Labelled label={props.label} required={props.required} >
-      <input
-        style={{
-          width: '100%',
+    <Labelled label={props.label} required={props.required}>
+      <Input
+        css={{
+          flex: "0 0 70%",
         }}
         name={props.name}
         type={props.type}
@@ -54,14 +69,14 @@ const ReCaptcha = () => {
   const siteKey = "6LeIP0oUAAAAANRB2QX0a3ItZkkiBJsmEs9pel4P"
   const secret = "HxjRkHBC9KGoa7rBLWS5L6mmWcIem/aJewy+hvao4gwXNengRVD+Xgjqffkt1JSzVr20wGWc1kG6RDx8y79kUyLGfcrUDro127Hvi+U7A8gnE4snDsXeYUPTnTxR0nbUqO4PmUApmNZf54IOtOyHZHmTFdV19/dvqJopL1jhByo="
   return (
-    <div>
+    <Div>
       <Helmet>
         <script src="https://www.google.com/recaptcha/api.js"></script>
       </Helmet>
-      <div className="g-recaptcha" data-sitekey={siteKey}></div>
+      <Div className="g-recaptcha" data-sitekey={siteKey}></Div>
       <FormOption option="reCaptcha.siteKey" value={siteKey} />
       <FormOption option="reCaptcha.secret" value={secret} />
-</div>
+    </Div>
   )
 }
 
@@ -71,55 +86,76 @@ class SubmitComment extends React.Component {
     const returnUrl = this.props.url
     const slug = this.props.slug
     return (
-      <div>
-        <form method="POST" action={formUrl} style={{
+      <Form method="POST" action={formUrl} css={{
+      }}>
+        <Legend css={{
+          fontSize: rhythm( 1 ),
+          flex: "0 0 100%",
+        }} >
+          Submit a Comment
+        </Legend>
+        <FormOption option="redirect" value={returnUrl} />
+        <FormOption option="slug"     value={slug} />
+
+        <LabelledInput
+          label="Name"
+          name="fields[name]"
+          type="text"
+          placeholder="Joe Cool"
+          required />
+
+        <LabelledInput
+          label="E-mail"
+          name="fields[email]"
+          type="email"
+          placeholder="joe.cool@example.com"
+          required />
+
+        <LabelledInput
+          label="Website"
+          name="fields[url]"
+          type="url"
+          placeholder="http://joecool.example.com/" />
+
+        <Labelled
+          label="Message"
+          required
+        >
+          <Textarea
+            name="fields[message]"
+            placeholder="I really like that the messages can be in **Markdown**."
+            css={{
+              flex: "0 1 70%",
+              width: "100%",
+              minWidth: rhythm( 10 ),
+            }} ></Textarea>
+        </Labelled>
+
+        <Labelled
+          label="I want to be notified of new comments"
+          divCss={{
+            display: "inline-block",
+            flex: "1 0 80%",
+          }}
+          required
+        >
+          <Div css={{
+            flex: "0 1 20%",
+            textAlign: "right",
+          }}>
+            <Input type="checkbox" name="options[subscribe]" css={{
+            }} />
+          </Div>
+        </Labelled>
+
+        <ReCaptcha />
+
+        <Div css={{
+          textAlign: "right",
         }}>
-          <legend style={{ fontSize: rhythm( 1 ) }} >
-            Submit a Comment
-          </legend>
-          <FormOption option="redirect" value={returnUrl} />
-          <FormOption option="slug"     value={slug} />
-
-          <LabelledInput
-            label="Name"
-            name="fields[name]"
-            type="text"
-            placeholder="Joe Cool"
-            required />
-
-          <LabelledInput
-            label="E-mail"
-            name="fields[email]"
-            type="email"
-            placeholder="joe.cool@example.com"
-            required />
-
-          <LabelledInput
-            label="Website"
-            name="fields[url]"
-            type="url"
-            placeholder="http://joecool.example.com/" />
-
-          <Labelled
-            label="Message"
-            required
-          >
-            <textarea name="fields[message]" placeholder="I really like that the messages can be in **Markdown**." >
-            </textarea>
-          </Labelled>
-
-          <Labelled
-            label="I want to be notified of new comments"
-            required
-          >
-            <input type="checkbox" name="options[subscribe]" value="email" className="checkbox" />
-          </Labelled>
-
-          <ReCaptcha />
-
-          <PrimaryButton>Comment</PrimaryButton>
-        </form>
-      </div>
+          <StyledButton>Comment</StyledButton>
+        </Div>
+      </Form>
     )
   }
 
