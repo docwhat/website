576f78c8647d1c423f5842d2978d3dce3745550b
Christian Höltje <docwhat@gerf.org>
2018-03-16 16:31:51 -0400

    removed rehype component stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,6 @@ module.exports = {
               wrapperStyle: `margin-bottom: 1.0725rem`,
             },
           },
-          'gatsby-remark-component',
           'gatsby-remark-copy-linked-files',
           'gatsby-remark-emoji',
           'gatsby-remark-smartypants',
