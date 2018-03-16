3a0ef4a8e80532bf033b2fdd134ef71aaf91f6e7
Christian Höltje <docwhat@gerf.org>
2018-03-16 16:20:16 -0400

    moved comments

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/src/comments`,
+        path: `${__dirname}/comments`,
         name: 'comments',
       },
     },
