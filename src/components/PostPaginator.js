27582788e2e0b5ba17707412ee582e63ee82d083
Christian Höltje <docwhat@gerf.org>
2018-03-22 11:03:08 -0400

    refactor

--- src/components/PostPaginator.js
+++ src/components/PostPaginator.js
@@ -5,6 +5,25 @@ import g from 'glamorous'
 import PropTypes from 'prop-types'
 import Link from './Link.js'
 import { IconArrowRight, IconArrowLeft } from './IconArrows.js'
+
+const Pager = ({ props, node, side, text }) => (
+  <g.Li css={{ margin: 0, padding: 0, flex: 1, textAlign: side }}>
+    <Link to={node.fields.slug}>
+      <div css={{ fontWeight: 'bold' }}>{text}</div>
+      <div
+        css={{
+          whiteSpace: 'nowrap',
+          overflow: 'hidden',
+          textOverflow: 'ellipsis',
+        }}
+      >
+        {side !== 'right' ? <IconArrowLeft /> : null} {node.fields.title}{' '}
+        {side === 'right' ? <IconArrowRight /> : null}
+      </div>
+    </Link>
+  </g.Li>
+)
+
 const PostPaginator = ({ newer, older }) => (
   <g.Nav
     css={{
@@ -20,26 +39,8 @@ const PostPaginator = ({ newer, older }) => (
         listStyleType: 'none',
       }}
     >
-      {older && (
-        <g.Li css={{ margin: 0, padding: 0, flex: 1 }}>
-          <Link to={older.fields.slug}>
-            <div css={{ fontWeight: 'bold' }}>Older</div>
-            <div>
-              <IconArrowLeft /> {older.fields.title}
-            </div>
-          </Link>
-        </g.Li>
-      )}
-      {newer && (
-        <g.Li css={{ margin: 0, padding: 0, flex: 1, textAlign: 'right' }}>
-          <Link to={newer.fields.slug}>
-            <div css={{ fontWeight: 'bold' }}>Newer</div>
-            <div>
-              {newer.fields.title} <IconArrowRight />
-            </div>
-          </Link>
-        </g.Li>
-      )}
+      {newer && <Pager node={newer} side={'left'} text={'Newer'} />}
+      {older && <Pager node={older} side={'right'} text={'Older'} />}
     </ul>
   </g.Nav>
 )
