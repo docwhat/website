4c2998f43857a5034270d09520abe2b8909c2640
Christian Höltje <docwhat@gerf.org>
2018-03-24 18:53:16 -0400

    tweaked markdown linter

--- .mdlstyle.rb
+++ .mdlstyle.rb
@@ -3,17 +3,18 @@
 
 all
 
-rule 'MD007', indent: 4
-rule 'MD029', style: 'ordered'
-rule 'MD004', style: :consistent
-rule 'MD035', style: '---'
+rule 'ul-style', style: :consistent  # MD004
+rule 'ul-indent', indent: 4          # MD007
+rule 'ol-prefix', style: 'ordered'   # MD029
+rule 'hr-style', style: '---'        # MD035
 
-exclude_rule 'first-header-h1'      # MD002
-exclude_rule 'fenced-code-language' # MD040
-exclude_rule 'first-line-h1'        # MD041
-exclude_rule 'header-style'         # MD029
-exclude_rule 'line-length'          # Line length
-exclude_rule 'list-marker-space'    # Spaces after list markers
-exclude_rule 'no-inline-html'       # MD033
+exclude_rule 'first-header-h1'       # MD002
+exclude_rule 'line-length'           # MD013
+exclude_rule 'header-style'          # MD029
+exclude_rule 'list-marker-space'     # MD030
+exclude_rule 'no-inline-html'        # MD033
+exclude_rule 'no-emphasis-as-header' # MD036
+exclude_rule 'fenced-code-language'  # MD040
+exclude_rule 'first-line-h1'         # MD041
 
 # EOF
