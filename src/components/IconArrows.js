c532695fb3307e632ae7a35060e4ed838c011c2a
Christian Höltje <docwhat@gerf.org>
2018-05-31 22:50:51 -0400

    disable eslint error
    I'm unclear how to add a display name with this code.

--- src/components/IconArrows.js
+++ src/components/IconArrows.js
@@ -4,6 +4,7 @@ import React from 'react'
 import { Svg } from 'glamorous'
 import { rhythm } from '../utils/typography'
 
+// eslint-disable-next-line react/display-name
 const IconArrow = overrideCss => () => (
   <Svg
     xmlns="http://www.w3.org/2000/svg"
