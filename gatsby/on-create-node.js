ce73490617f99a7cab8a3da2e08733a6cc9949d2
Christian Höltje <docwhat@gerf.org>
2019-02-21 00:46:25 -0500

    chore(onCreateNode): cleaner style

--- gatsby/on-create-node.js
+++ gatsby/on-create-node.js
@@ -4,64 +4,53 @@ const pathlib = require(`path`)
 const _ = require(`lodash`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
-const onCreateNode = ({ node, actions, getNode }) => {
-  try {
-    const { createNodeField } = actions
-
-    if (node.internal.type === `MarkdownRemark`) {
-      const sourceName = getNode(node.parent).sourceInstanceName
-
-      createNodeField({ node, name: `sourceName`, value: sourceName })
-      if (sourceName === `comments`) {
-        createNodeField({
-          node,
-          name: `postSlug`,
-          value: replacePath(node.frontmatter.slug),
-        })
-        return
-      }
-
-      const slug = calculateSlug(node, getNode)
-
-      createNodeField({
-        node,
-        name: `slug`,
-        value: slug,
-      })
-      createNodeField({
-        node,
-        name: `title`,
-        value: calculateTitle(node, getNode),
-      })
-      if (sourceName === 'posts') {
-        const archived =
-          _.get(node, 'frontmatter.archive', false) ||
-          _.get(node, 'frontmatter.archived', false)
-        createNodeField({
-          node,
-          name: `archived`,
-          value: archived,
-        })
-        const draft = _.get(node, `frontmatter.draft`, false)
-        createNodeField({
-          node,
-          name: `draft`,
-          value: draft,
-        })
-      }
-      if (sourceName !== 'pages') {
-        createNodeField({
-          node,
-          name: `date`,
-          value: calculateDate(node, getNode, sourceName),
-        })
-      }
-    }
-  } catch (ex) {
-    // eslint-disable-next-line no-console
-    console.error(`Error onCreateNode():`, node.fileAbsolutePath, `\n`, ex)
-    throw ex
+const onCreateNode = async ({ node, actions, getNode }) => {
+  if (node.internal.type !== `MarkdownRemark`) {
+    return {}
   }
+
+  const { createNodeField } = actions
+  const sourceName = getNode(node.parent).sourceInstanceName
+
+  createNodeField({ node, name: `sourceName`, value: sourceName })
+
+  const slug = calculateSlug(node, getNode)
+
+  createNodeField({
+    node,
+    name: `slug`,
+    value: slug,
+  })
+  createNodeField({
+    node,
+    name: `title`,
+    value: calculateTitle(node, getNode),
+  })
+  if (sourceName === 'posts') {
+    const archived =
+      _.get(node, 'frontmatter.archive', false) ||
+      _.get(node, 'frontmatter.archived', false)
+    createNodeField({
+      node,
+      name: `archived`,
+      value: archived,
+    })
+    const draft = _.get(node, `frontmatter.draft`, false)
+    createNodeField({
+      node,
+      name: `draft`,
+      value: draft,
+    })
+  }
+  if (sourceName !== 'pages') {
+    createNodeField({
+      node,
+      name: `date`,
+      value: calculateDate(node, getNode, sourceName),
+    })
+  }
+
+  return node
 }
 
 const calculateTitle = (node, getNode) =>
