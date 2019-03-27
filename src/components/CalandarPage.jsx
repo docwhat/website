acf87be5868ddc651eccced03e904f0455ed00f1
Christian Höltje <docwhat@gerf.org>
2019-03-27 18:29:24 -0400

    feat(cal): add localized month

--- src/components/CalandarPage.jsx
+++ src/components/CalandarPage.jsx
@@ -7,85 +7,110 @@ import { heroColor } from '../utils/colors'
 import { getNavigatorLanguage, ymdString2Date } from '../utils/dates.js'
 import { rhythm } from '../utils/typography'
 
-const CalendarPage = (props: { ymdDate: string }) => {
-  const { ymdDate } = props
-  const date = ymdString2Date(ymdDate)
+type State = {
+  monthName: string,
+}
 
-  const commonCSS = {
-    display: `block`,
-    fontSize: `1em`,
-    fontStyle: `normal`,
-    fontWeight: `bold`,
-    textAlign: `center`,
-    width: `100%`,
+type Props = {
+  ymdDate: string,
+}
+
+class CalendarPage extends React.Component<Props, State> {
+  state = {
+    monthName: '',
   }
 
-  const bannerColor = heroColor.darken(0.0)
-  const fontColor = bannerColor.negate()
+  componentDidMount() {
+    const date = ymdString2Date(this.props.ymdDate)
+    const lang = getNavigatorLanguage()
 
-  const rmult = 5 / 12
-  const lang = getNavigatorLanguage()
-  const year = date.getFullYear()
-  const dayOfMonth = date.getDate()
-  const monthName = date.toLocaleDateString(lang, { month: 'long' })
+    this.setState({
+      monthName: date.toLocaleDateString(lang, {
+        month: 'short',
+      }),
+    })
+  }
 
-  // Original idea from:
-  // https://www.sitepoint.com/create-calendar-icon-html5-css3/
-  return (
-    <time
-      css={{
-        backgroundColor: `#ffffff`,
-        borderRadius: rhythm(0.6 * rmult),
-        boxShadow: `0 1.0px 0 #bdbdbd, 0 2px 0 #fff, 0 3px 0 #bdbdbd, 0 4px 0 #fff, 0 5px 0 #bdbdbd, 0 0 0 1.0px #bdbdbd`,
-        display: `block`,
-        fontSize: rhythm(rmult),
-        /* change icon size */
-        height: rhythm(7 * rmult),
-        margin: 0,
-        overflow: `hidden`,
-        position: `relative`,
-        width: rhythm(7 * rmult),
-      }}
-      dateTime={ymdDate}
-      title={ymdDate}
-    >
-      <em
+  render() {
+    const { ymdDate } = this.props
+    const date = ymdString2Date(ymdDate)
+
+    const commonCSS = {
+      display: `block`,
+      fontSize: `1em`,
+      fontStyle: `normal`,
+      fontWeight: `bold`,
+      textAlign: `center`,
+      width: `100%`,
+    }
+
+    const bannerColor = heroColor.darken(0.0)
+    const fontColor = bannerColor.negate()
+
+    const rmult = 5 / 12 // Magic number... I know... horrible.
+    const lang = getNavigatorLanguage()
+    const year = date.getFullYear()
+    const dayOfMonth = date.getDate()
+    const monthName = date.toLocaleDateString(lang, { month: 'short' })
+
+    // Original idea from:
+    // https://www.sitepoint.com/create-calendar-icon-html5-css3/
+    return (
+      <time
         css={{
-          ...commonCSS,
-          bottom: rhythm(0.3 * rmult),
-          color: bannerColor,
-          position: `absolute`,
+          backgroundColor: `#ffffff`,
+          borderRadius: rhythm(0.6 * rmult),
+          boxShadow: `0 1.0px 0 #bdbdbd, 0 2px 0 #fff, 0 3px 0 #bdbdbd, 0 4px 0 #fff, 0 5px 0 #bdbdbd, 0 0 0 1.0px #bdbdbd`,
+          display: `block`,
+          fontSize: rhythm(rmult),
+          /* change icon size */
+          height: rhythm(7 * rmult),
+          margin: 0,
+          overflow: `hidden`,
+          position: `relative`,
+          width: rhythm(7 * rmult),
         }}
+        dateTime={ymdDate}
+        title={ymdDate}
       >
-        {monthName}
-      </em>
-      <strong
-        css={{
-          ...commonCSS,
-          backgroundColor: bannerColor.string(),
-          borderBottom: `1.0px dashed ${bannerColor.darken(0.2).string()}`,
-          boxShadow: `0 2px 0 ${bannerColor.string()}`,
-          color: fontColor,
-          padding: `${rhythm(0.4 * rmult)} 0`,
-          position: `absolute`,
-          top: `0`,
-        }}
-      >
-        {year}
-      </strong>
-      <span
-        css={{
-          ...commonCSS,
-          color: `#2f2f2f`,
-          fontSize: rhythm(2.0 * rmult),
-          letterSpacing: rhythm(-0.05 * rmult),
-          paddingTop: rhythm(2.6 * rmult),
-        }}
-      >
-        {dayOfMonth}
-      </span>
-    </time>
-  )
+        <strong
+          css={{
+            ...commonCSS,
+            backgroundColor: bannerColor.string(),
+            borderBottom: `1.0px dashed ${bannerColor.darken(0.2).string()}`,
+            boxShadow: `0 2px 0 ${bannerColor.string()}`,
+            color: fontColor,
+            padding: `${rhythm(0.4 * rmult)} 0`,
+            position: `absolute`,
+            top: `0`,
+          }}
+        >
+          {year}
+        </strong>
+        <em
+          css={{
+            ...commonCSS,
+            bottom: rhythm(0.3 * rmult),
+            color: bannerColor,
+            position: `absolute`,
+          }}
+        >
+          {this.state.monthName === '' ? monthName : this.state.monthName}
+        </em>
+        <span
+          css={{
+            ...commonCSS,
+            color: `#2f2f2f`,
+            fontSize: rhythm(2.0 * rmult),
+            letterSpacing: rhythm(-0.05 * rmult),
+            paddingTop: rhythm(2.6 * rmult),
+          }}
+        >
+          {dayOfMonth}
+        </span>
+      </time>
+    )
+  }
 }
 
 export default CalendarPage
