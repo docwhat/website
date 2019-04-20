e398afd5a98e290a66e56d40f0af6b40ae3f2529
Christian Höltje <docwhat@gerf.org>
2019-04-19 21:06:02 -0400

    lighten the small print to be less visible

--- src/components/SmallPrint.jsx
+++ src/components/SmallPrint.jsx
@@ -11,11 +11,12 @@ type Props = {
 const SmallPrint = ({ children, ...other }: Props): React.Node => (
   <small
     css={{
+      fontSize: '65%',
       fontStyle: 'italic',
-      fontSize: '70%',
       lineHeight: 1,
       marginBottom: rhythm(1 / 4),
       marginTop: rhythm(1 / 4),
+      opacity: 0.5,
       whiteSpace: 'pre',
     }}
     {...other}
