dfc8ba485944bedc2b6a31f75d5e2ec06d5cd819
Christian Höltje <docwhat@gerf.org>
2019-04-20 16:01:23 -0400

    chore(LinkButton): use emotion instead of whole react component

--- src/components/LinkButton.jsx
+++ src/components/LinkButton.jsx
@@ -1,57 +1,42 @@
 // @flow
 // @format
 
+import styled from '@emotion/styled'
 import Link from 'gatsby-link'
-import * as React from 'react'
 
 import { heroColor } from '../utils/colors.js'
 import { rhythm } from '../utils/typography.js'
 
-type Props = {
-  to: string,
-  children: React.Node,
-}
-
 const color = heroColor.negate()
 const background = heroColor
 
-const LinkButton = ({ children, to, ...other }: Props): React.Node => (
-  <Link
-    css={{
-      display: `block`,
-      textAlign: 'center',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      fontSize: rhythm(1 / 2),
-      backgroundColor: background
-        .desaturate(0.3)
-        .lighten(0.3)
-        .string(),
-      color: color
-        .desaturate(0.3)
-        .lighten(0.3)
-        .string(),
-      userSelect: 'none',
-      cursor: 'pointer',
-      height: rhythm(1 + 1 / 4),
-      lineHeight: rhythm(1 + 1 / 4),
-      width: rhythm(5),
-      borderRadius: rhythm(1),
-      '&:hover': {
-        textDecoration: 'none',
-        boxShadow: `1px 1px 4px rgba(0, 0, 0, 0.5)`,
-        backgroundColor: background.string(),
-        color: color.string(),
-      },
-      '&:active': {
-        boxShadow: `0 0 2px rgba(0, 0, 0, 0.8)`,
-      },
-    }}
-    to={to}
-    {...other}
-  >
-    {children}
-  </Link>
-)
-
-export default LinkButton
+export default styled(Link)`
+  display: block;
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  font-size: ${rhythm(1 / 2)};
+  background-color: ${background
+    .desaturate(0.3)
+    .lighten(0.3)
+    .string()};
+  color: ${color
+    .desaturate(0.3)
+    .lighten(0.3)
+    .string()};
+  user-select: none;
+  cursor: pointer;
+  height: ${rhythm(1 + 1 / 4)};
+  line-height: ${rhythm(1 + 1 / 4)};
+  width: ${rhythm(5)};
+  border-radius: ${rhythm(1)};
+  &:hover {
+    text-decoration: none;
+    box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.5);
+    background-color: ${background.string()};
+    color: ${color.string()};
+  }
+  &:active {
+    box-shadow: 0 0 2px rgba(0, 0, 0, 0.8);
+  }
+`
