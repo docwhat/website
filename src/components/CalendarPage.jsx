fb4f3c5c95f799691a8787ad39012a98f66a6798
Christian Höltje <docwhat@gerf.org>
2019-05-04 23:58:45 -0400

    chore(CalendarPage): convert css to emotion

--- src/components/CalendarPage.jsx
+++ src/components/CalendarPage.jsx
@@ -1,5 +1,6 @@
 // @flow
 // @format
+import styled from '@emotion/styled'
 import { graphql } from 'gatsby'
 import React from 'react'
 
@@ -35,15 +36,6 @@ class CalendarPage extends React.Component<Props, State> {
     const { ymdDate } = this.props
     const date = ymdString2Date(ymdDate)
 
-    const commonCSS = {
-      display: `block`,
-      fontSize: `1em`,
-      fontStyle: `normal`,
-      fontWeight: `bold`,
-      textAlign: `center`,
-      width: `100%`,
-    }
-
     const bannerColor = heroColor.darken(0.0)
     const fontColor = bannerColor.negate()
 
@@ -52,62 +44,64 @@ class CalendarPage extends React.Component<Props, State> {
     const dayOfMonth = date.getDate()
     const monthName = date.toLocaleDateString('en-US', { month: 'short' })
 
+    const Wrapper = styled.time`
+      background-color: #ffffff;
+      border-radius: ${rhythm(0.6 * rmult)};
+      box-shadow: 0 1px 0 #bdbdbd, 0 2px 0 #fff, 0 3px 0 #bdbdbd, 0 4px 0 #fff,
+        0 5px 0 #bdbdbd, 0 0 0 1px #bdbdbd;
+      display: block;
+      font-size: ${rhythm(rmult)};
+
+      /* change icon size */
+      height: ${rhythm(7 * rmult)};
+      margin: 0;
+      overflow: hidden;
+      position: relative;
+      width: ${rhythm(7 * rmult)};
+
+      // CommonCSS
+      & > * {
+        display: block;
+        font-size: 1em;
+        font-style: normal;
+        font-weight: bold;
+        text-align: center;
+        width: 100%;
+      }
+    `
+
+    const Banner = styled.div`
+      background-color: ${bannerColor.string()};
+      border-bottom: 1px dashed ${bannerColor.darken(0.2).string()};
+      box-shadow: 0 2px 0 ${bannerColor.string()};
+      color: ${fontColor.string()};
+      padding: ${rhythm(0.4 * rmult)} 0;
+      position: absolute;
+      top: 0;
+    `
+
+    const Footer = styled.div`
+      bottom: ${rhythm(0.3 * rmult)};
+      color: ${fontColor.string()};
+      position: absolute;
+    `
+    const Body = styled.div`
+      color: #2f2f2f;
+      font-size: ${rhythm(2.0 * rmult)};
+      letter-spacing: ${rhythm(-0.05 * rmult)};
+      padding-top: ${rhythm(2.6 * rmult)};
+    `
+
     // Original idea from:
     // https://www.sitepoint.com/create-calendar-icon-html5-css3/
     return (
-      <time
-        css={{
-          backgroundColor: `#ffffff`,
-          borderRadius: rhythm(0.6 * rmult),
-          boxShadow: `0 1.0px 0 #bdbdbd, 0 2px 0 #fff, 0 3px 0 #bdbdbd, 0 4px 0 #fff, 0 5px 0 #bdbdbd, 0 0 0 1.0px #bdbdbd`,
-          display: `block`,
-          fontSize: rhythm(rmult),
-          /* change icon size */
-          height: rhythm(7 * rmult),
-          margin: 0,
-          overflow: `hidden`,
-          position: `relative`,
-          width: rhythm(7 * rmult),
-        }}
-        dateTime={ymdDate}
-        title={ymdDate}
-      >
-        <strong
-          css={{
-            ...commonCSS,
-            backgroundColor: bannerColor.string(),
-            borderBottom: `1.0px dashed ${bannerColor.darken(0.2).string()}`,
-            boxShadow: `0 2px 0 ${bannerColor.string()}`,
-            color: fontColor,
-            padding: `${rhythm(0.4 * rmult)} 0`,
-            position: `absolute`,
-            top: `0`,
-          }}
-        >
-          {year}
-        </strong>
-        <em
-          css={{
-            ...commonCSS,
-            bottom: rhythm(0.3 * rmult),
-            color: bannerColor,
-            position: `absolute`,
-          }}
-        >
+      <Wrapper dateTime={ymdDate} title={ymdDate}>
+        <Banner>{year}</Banner>
+        <Body>{dayOfMonth}</Body>
+        <Footer>
           {this.state.monthName === '' ? monthName : this.state.monthName}
-        </em>
-        <span
-          css={{
-            ...commonCSS,
-            color: `#2f2f2f`,
-            fontSize: rhythm(2.0 * rmult),
-            letterSpacing: rhythm(-0.05 * rmult),
-            paddingTop: rhythm(2.6 * rmult),
-          }}
-        >
-          {dayOfMonth}
-        </span>
-      </time>
+        </Footer>
+      </Wrapper>
     )
   }
 }
